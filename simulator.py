46eb6b2d1fc5d324ba067b1c73473de90fea139f
Fratealin <61327134+Fratealin@users.noreply.github.com>
2020-04-28 10:59:30 +0100

    Update simulator.py
    clean up simulator

--- simulator.py
+++ simulator.py
@@ -1,19 +1,14 @@
 import random
 
 
-
-
-def advance_one_week(people, population, infected_people):
-
+def advance_one_week(people):
     infected_people = []
-
     for person in people:
         risk_percentage = random.random()*10
         if person.riskFactor >= risk_percentage:
             person.infection = True
         if person.infection:
             infected_people.append(person.name)
-
 
     print(f"{len(infected_people)} people have got the virus:\n{', '.join(infected_people)}")
     return infected_people
